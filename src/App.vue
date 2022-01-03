ac1ee32ed469632b3e6fb508e5430c3108059944
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2022-01-03 23:47:16 +0500

    steps

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <form class="d-flex mt-2" action="">
+
+    <form class="mt-2" @submit.prevent>
       <div class="font-bold mx-4 my-2">Создание</div>
-      <input class="" type="text" placeholder="Название">
-      <input class="focus:outline" type="text" placeholder="Описание">
-      <button class="btn mx-4 text-center bg-purple-200 hover:shadow-purple-900 hover:outline hover:ring-2 ring-purple-400" >Создать</button>
+      <div class="mx-4 my-2">
+        <input
+            v-bind:value="title"
+            @input="title = $event.target.value"
+            class="focus:outline ring-purple-400 hover:outline" type="text" placeholder="Название">
+      </div>
+      <div class="mx-4 my-2">
+        <input
+            v-bind:value="body"
+            @input="body = $event.target.value"
+            class="focus:outline ring-purple-400 hover:outline" type="text" placeholder="Описание">
+      </div>
+        <button @click="createNewPost" class="btn mx-4 text-center bg-purple-200 hover:shadow-purple-900 hover:outline hover:ring-2 ring-purple-400">
+          Создать
+        </button>
     </form>
     <div class="post border-2 border-indigo-600 m-4 p-2 " v-for="post in posts">
       <div><span class="font-bold">Название: </span>{{ post.title }}</div>
@@ -17,6 +30,8 @@
 export default {
   data() {
     return {
+      title: '',
+      body: '',
       posts: [
         {id: 1, title: 'Title 1', body: 'Body 1'},
         {id: 2, title: 'Title 1', body: 'Body 1'},
@@ -25,6 +40,15 @@ export default {
     }
   },
   methods: {
+
+    createNewPost(){
+      const newPost = {
+       id: Date.now(),
+       title: this.title,
+       body: this.title,
+      }
+      this.posts.push(newPost);
+    },
     addLike() {
       this.likes += 1;
     },
@@ -40,7 +64,8 @@ export default {
   padding: 5px 15px;
   border-radius: 5px;
 }
+
 input {
-  @apply py-2 px-5 bg-purple-200
+  @apply py-2 px-5 bg-purple-200 rounded
 }
 </style>
---
+++ src/Second.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Title 1', body: 'Body 1'},
+        {id: 2, title: 'Title 1', body: 'Body 1'},
+        {id: 3, title: 'Title 1', body: 'Body 1'},
+      ]
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes -= 1;
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+.btn {
+  padding: 5px 15px;
+  border-radius: 5px;
+}
+input {
+  @apply py-2 px-5 bg-purple-200
+}
+</style>
